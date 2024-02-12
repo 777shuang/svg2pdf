a00e9140808261bc2359bb4ca35513a0ee1b553a
777shuang <100687590+777shuang@users.noreply.github.com>
2024-02-12 21:55:13 +0900

    Update inkscape.cpp

--- inkscape.cpp
+++ inkscape.cpp
@@ -7,7 +7,7 @@
 
 void option_error()
 {
-    std::cout << "Invalid option" << std::endl << "inkscape -z --file=input.svg --export-pdf=output.pdf";
+    std::cout << "Invalid option" << std::endl << "svg2pdf input.svg output.pdf";
     std::exit(EXIT_FAILURE);
 }
 
@@ -16,25 +16,6 @@ int main(int argc, char* argv[])
     std::string filename = "";
     std::string output = "";
 
-    if (argc == 4)
-    {
-        std::array<std::string, 3> args;
-        for (std::uint8_t i = 1; i < argc; i++) { args[i - 1] = argv[i]; }
-
-        if (args[0] != "-z") { option_error(); }
-        if (args[1].starts_with("--file=")) { filename = args[1].substr(7); }
-        else { option_error(); }
-        if (args[2].starts_with("--export-pdf")) { output = args[2].substr(13); }
-        else { option_error(); }
-    
-        std::cout << filename << std::endl << output << std::endl;
-    
-        return svg2pdf(filename.c_str(), output.c_str());
-    }
-    else if (argc == 1)
-    {
-        std::cout << "Inkscape 1.3.2 (091e20e, 2023-11-25, custom)" << std::endl; // dummy string
-        return EXIT_SUCCESS;
-    }
+    if (argc == 3) { return svg2pdf(argv[1], argv[2]); }
     else { option_error(); }
 }
